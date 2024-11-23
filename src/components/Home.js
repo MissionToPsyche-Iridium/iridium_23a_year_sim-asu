c2b0f769bb23d59c4638466474473bbb34d33013
tanavjalan2003 <161883518+tanavjalan2003@users.noreply.github.com>
2024-11-23 15:48:52 -0700

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,59 +1,24 @@
-import React, { useState } from 'react';
-import { Helmet } from 'react-helmet';  //Helmet to change page title
-import '../styles.css';  
+import React from 'react';
+import { Helmet } from 'react-helmet'; // Helmet to change page title
+import '../styles.css';
 
 const Home = () => {
-  // State to handle the active button and iframe source
-  const [activeButton, setActiveButton] = useState('System View');
-  const [iframeSrc, setIframeSrc] = useState('Simulation_OrbitalView.html'); // Default iframe source
-
-  // Function to handle image change and iframe source update
-  const changeView = (buttonLabel, iframeSource) => {
-    setActiveButton(buttonLabel);  // Set active button
-
-    // Change iframe source to switch views
-    setIframeSrc(iframeSource); 
-  };
-
   return (
     <main>
-        <Helmet>
-            <title>Home - Psyche Simulation</title>
-        </Helmet>
+      <Helmet>
+        <title>Home - Psyche Simulation</title>
+      </Helmet>
       <div className="home-page">
         <h2>Welcome to the Year on Psyche Simulation</h2>
-        <p>Click on the below buttons to change your view of Psyche</p>
+        <p>Experience the views of Psyche directly below.</p>
       </div>
 
-      <div className="button-image-container">
-        <div className="button-group">
-          <button 
-            onClick={() => changeView('System View', 'Simulation_OrbitalView.html')}
-            className={activeButton === 'System View' ? 'button-active' : ''}
-          >
-            System View
-          </button>
-
-          <button 
-            onClick={() => changeView('Orbital View', 'Simulation_OrbitalView.html')}
-            className={activeButton === 'Orbital View' ? 'button-active' : ''}
-          >
-            Orbital View
-          </button>
-
-          <button 
-            onClick={() => changeView('Terrestrial View', 'Simulation_TerrestrialView.html')}
-            className={activeButton === 'Terrestrial View' ? 'button-active' : ''}
-          >
-            Terrestrial View
-          </button>
-        </div>
-
-        <iframe 
-          src={iframeSrc}  
-          name="Simulation_Iframe" 
-          title="Simulation of a Year on the Asteroid Psyche" 
-          style={{ width: '750px', height: '250px', border: 'none' }}
+      <div className="iframe-container">
+        <iframe
+          src="Simulation_OrbitalView.html" // Update to the default or desired source
+          name="Simulation_Iframe"
+          title="Simulation of a Year on the Asteroid Psyche"
+          style={{ width: '80vw', height: '50vh', border: 'none' }}
         />
       </div>
     </main>
